ac472d2d6364b518d4a0b424c272b403e2cc8545
hendisantika <hendisantika@yahoo.co.id>
2025-10-15 12:44:15 +0700

    feat: Add HibernateConfiguration 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/companystructure/config/HibernateConfiguration.java
@@ -0,0 +1,35 @@
+package id.my.hendisantika.companystructure.config;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-security-oauth2-authorities-company-structure
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/10/25
+ * Time: 12.43
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableTransactionManagement
+public class HibernateConfiguration {
+
+    @Bean
+    public Module hibernate5Module() {
+        return new Hibernate5Module();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+
+        return transactionManager;
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+}
