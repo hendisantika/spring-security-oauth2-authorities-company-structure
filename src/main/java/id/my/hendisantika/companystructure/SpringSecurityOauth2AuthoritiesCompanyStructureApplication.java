469f79bfb1d2951eab4bb6fcf48d92d7abb83908
hendisantika <hendisantika@yahoo.co.id>
2025-10-15 12:35:30 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/companystructure/SpringSecurityOauth2AuthoritiesCompanyStructureApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.companystructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth2AuthoritiesCompanyStructureApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOauth2AuthoritiesCompanyStructureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/companystructure/SpringSecurityOauth2AuthoritiesCompanyStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.companystructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth2AuthoritiesCompanyStructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
