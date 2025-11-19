85b2b12477445416afb8dce8be344f6e1509f016
hendisantika <hendisantika@yahoo.co.id>
2025-11-20 06:42:10 +0700

    feat: Add Company Model🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/companystructure/model/Company.java
@@ -0,0 +1,69 @@
+package id.my.hendisantika.companystructure.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-security-oauth2-authorities-company-structure
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 20/11/25
+ * Time: 06.41
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "COMPANY", uniqueConstraints = {@UniqueConstraint(columnNames = {"NAME"})})
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class Company implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID", updatable = false, nullable = false)
+    private Long id = null;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "company", fetch = FetchType.LAZY, orphanRemoval = true)
+    @JsonManagedReference
+    private Set<Department> departments = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "company", fetch = FetchType.LAZY, orphanRemoval = true)
+    @JsonManagedReference
+    private Set<Car> cars = new HashSet<>();
+
+    public void setDepartments(Set<Department> departments) {
+        this.departments.clear();
+        if (departments != null) {
+            this.departments.addAll(departments);
+        }
+    }
+
+    public void setCars(Set<Car> cars) {
+        this.cars.clear();
+        if (cars != null) {
+            this.cars.addAll(cars);
+        }
+    }
+}
