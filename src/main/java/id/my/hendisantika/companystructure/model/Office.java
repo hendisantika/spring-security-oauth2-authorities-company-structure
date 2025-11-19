a34db1fd689a05bdd2920207375b4f98e93c8c97
hendisantika <hendisantika@yahoo.co.id>
2025-11-20 06:44:43 +0700

    feat: Add Office Model🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/companystructure/model/Office.java
@@ -0,0 +1,52 @@
+package id.my.hendisantika.companystructure.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-security-oauth2-authorities-company-structure
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 20/11/25
+ * Time: 06.44
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "OFFICE")
+@Getter
+@Setter
+public class Office implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID", updatable = false, nullable = false)
+    private Long id = null;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "ADDRESS_ID")
+    private Address address;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonBackReference
+    private Department department;
+}
